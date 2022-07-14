ad2b17963d04c8ab4509888c8cfe33b75a407ec0
Juan David <juandavidcarrillogarcia@gmail.com>
2022-07-14 10:15:25 -0500

    Primeros pasos en php uso del comando print y echo

---
+++ index.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola mundo</h1>
+    <?php
+
+       echo "<p>Gracias por ver el curso :) </p>";
+       echo "<p style='color:blue'>los espero en el siguiente curso</p>"
+
+    ?>
+
+    
+</body>
+</html>
+
+<?php
+
+
+  //Mi primer comentario
+  # Otro comentario
+  /* 
+     Un bloque de comentario mas grande
+     :
+
+  */
+
+
+
+  /*
+
+  // Como imprimir mensaje en php
+  print "Hola mundo";
+  echo "Mi primer Hola Mundo en Ed Team\n";
+
+  // el comando eco nos permite con catenar
+
+  echo "Hola ","mundo";
+
+  // con php podemos colocar codigo de html adentro
+  echo "<h1>Hola a todos</h1>";
+  */
+
+
+?>
