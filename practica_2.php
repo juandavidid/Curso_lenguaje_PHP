41784b4497e84e9e99a93ec807e8a4160c581be4
Juan David <juandavidcarrillogarcia@gmail.com>
2022-07-19 17:12:38 -0500

    Se hizo un 1 ejercicio de arreglos(vectores)

---
+++ practica_2.php
@@ -0,0 +1,26 @@
+<?php
+/*
+EJERCICIO 1
+ Crea un arreglo que contenga como clave los nombres de
+ 5 países y como valor otro arreglo con 3 ciudades que 
+ pertenezcan a ese país, después utiliza un ciclo foreach, 
+ para imprimir el nombre del país seguido de las ciudades que definiste:
+
+Ejemplo,
+
+México: Monterrey Querétaro Guadalajara
+Colombia: Bogota Cartagena Medellin
+
+*/
+
+# SOLUCION
+
+$pais_ciudades = array( 'colombia' => ['Bogota','Cali','Medellin'],'Argentina' => ['Bueno Aires','Salta','La plata']  );
+
+foreach($pais_ciudades as $clave => $valor){
+    #echo "<p>$clave - $valor</p>";
+    #var_dump ($clave,$pais_ciudades[$clave]);
+    var_dump ($clave,$valor);
+}
+
+?>
