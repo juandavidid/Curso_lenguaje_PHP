bd2e9c939d95439a416f001c7a96831348ed1a07
Juan David PC_2 <juandavidcarrillogarcia@gmail.com>
2022-08-25 17:19:14 -0500

    Se estudio la estructura switch

---
+++ condicional_if_switch/condicionales.php
@@ -0,0 +1,44 @@
+<?php
+ /*
+ TEMA : CONDICIONAL SWITVH
+
+ switch (condicion) {
+
+      case x:
+        bloque de codigo
+        break ;
+      defautl:
+         bloque de codigo
+         break;
+        
+
+ } 
+
+ */
+
+ // Ejemplo
+
+ $numero = 3;
+
+ switch ($numero) {
+    case 1 :
+        echo "El numero es igual a 1 <br />";
+        break ;
+    case 2 :
+        echo "El numero es igual a 2 <br />";
+        break;
+    case 3 :
+        echo "El numero es igual a 3 <br />";
+        break;
+    default :
+        echo "Es otro numero <br />";
+        break ; 
+
+ }
+
+
+
+
+
+
+?>
---
+++ condicional_if_switch/condicionales_p2.php
@@ -0,0 +1,15 @@
+<?php
+
+$numero = "100";
+
+if ($numero === 100){
+    echo "Es identico";
+}else {
+    echo "No es identico";
+}
+
+
+
+
+
+?>
